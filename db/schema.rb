7280f8bdaa24929653ed5dca5826ef0ea8d562bf
Rusli <tan.rusli@outlook.com>
2018-09-12 21:31:46 +1000

    devise

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180912112313) do
+ActiveRecord::Schema.define(version: 20180912112909) do
 
   create_table "conversations", force: :cascade do |t|
     t.integer  "recipient_id"
@@ -29,6 +29,18 @@ ActiveRecord::Schema.define(version: 20180912112313) do
     t.datetime "updated_at",      null: false
     t.index ["conversation_id"], name: "index_messages_on_conversation_id"
     t.index ["user_id"], name: "index_messages_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
